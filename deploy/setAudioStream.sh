d6fc22a450b5883e3fbdb4209aafe337027a9d60
Fabian Markert <f.markert87@gmail.com>
2021-02-09 02:46:30 +0100

    script Warnungen beseitigt

--- deploy/setAudioStream.sh
+++ deploy/setAudioStream.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+echo "replace Stream url"
 STREAM_URL="https:\/\/fm4.stream.laut.fm\/fm4"
 PLACEHOLDER="--streamurl--"
-echo "${PLACEHOLDER}"
-sed -i  "s/$PLACEHOLDER/$STREAM_URL/g" bc.json
+
+sed -i  "s/$PLACEHOLDER/$STREAM_URL/g" *.json
