18556c91f90dc9ed094183f8eea019980cbd0b0f
Fabian Markert <f.markert87@googlemail.com>
2021-02-15 09:34:25 +0100

    Update setAudioStream.sh
    urls korrigiert

--- deploy/setAudioStream.sh
+++ deploy/setAudioStream.sh
@@ -8,10 +8,10 @@ TZ=Europe/Berlin
 export TZ
 DOW=$(date +%u)
 
-STREAM_DAY_URLS[1]="https://stream.radioparadise.com/mp3-128"
-STREAM_NIGHT_URLS[1]="https://streams.radiobob.de/100/mp3-192/streams.radiobob.de/"
-STREAM_DAY_URLS[2]="https://stream.radioparadise.com/mp3-128"
-STREAM_NIGHT_URLS[2]="https://br-puls-live.cast.addradio.de/br/puls/live/mp3/mid"
+STREAM_DAY_URLS[1]="https:\/\/stream.radioparadise.com\/mp3-128"
+STREAM_NIGHT_URLS[1]="https://streams.radiobob.de\/100\/mp3-192\/streams.radiobob.de\/"
+STREAM_DAY_URLS[2]="https:\/\/stream.radioparadise.com\/mp3-128"
+STREAM_NIGHT_URLS[2]="https:\/\/br-puls-live.cast.addradio.de\/br\/puls\/live\/mp3\/mid"
 STREAM_DAY_URLS[3]="https:\/\/stream.radioparadise.com\/mp3-128"
 STREAM_NIGHT_URLS[3]="https:\/\/streams.starfm.de\/hot_top.mp3"
 STREAM_DAY_URLS[4]="https:\/\/stream.radioparadise.com\/mp3-128"
